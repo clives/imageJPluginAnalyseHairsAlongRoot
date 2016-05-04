b67872e0e3bbe5456542518e65093181be39d5c1
cliveUbuntuAsusN <clive@romandie.com>
2016-05-04 17:10:08 -0300

    first recursive version

--- src/test/scala/testRegroupFuzzyHairPixel.scala
+++ src/test/scala/testRegroupFuzzyHairPixel.scala
@@ -3,9 +3,9 @@ import ij.IJ
 import algo.SearchHairs._
 import algo._
 import algo.SearchHairs._
-
 import imagej.tools._
-  import java.awt.Color._
+import java.awt.Color._
+import ij.ImagePlus
 /*
  * 
  **/
@@ -76,6 +76,8 @@ class testRegroupFuzzyHairPixel extends WordSpec {
       }
       
       // once we detected a hair, with have to remove all the pixels around
+      // for the moment, only the ones from the left to the right, not delta up / down...
+      // to fix it.
       @tailrec
       def removeHairPixels( pixelstocheck: List[HairPixelDelta], allpixelhairs: DeltaHairs, pixeltoremove: List[HairPixelDelta] =List.empty):List[HairPixelDelta] ={
         
@@ -88,7 +90,7 @@ class testRegroupFuzzyHairPixel extends WordSpec {
           //visit if not present in pixelstocheck and pixeltoremove and prHair > 0.5
           val nextToVisit=(allpixelhairs.get(currentpixel.delta).toList.flatten.filter{p=> neighbours contains p.x}.map{
             p => new HairPixelDelta(p, currentpixel.delta)
-          }).filter (_.hp.prHair > 0.3).filterNot( pixeltoremove contains _).filterNot( pixelstocheck contains _)
+          }).filter (_.hp.prHair > 0.1).filterNot( pixeltoremove contains _).filterNot( pixelstocheck contains _)
           
           removeHairPixels( pixelstocheck.tail ++ nextToVisit, allpixelhairs, currentpixel::pixeltoremove)
         }
@@ -103,8 +105,6 @@ class testRegroupFuzzyHairPixel extends WordSpec {
       def consume(currentpixel: HairPixelDelta, hairs: DeltaHairs, deltas: Range, currentPixel: List[HairPixelDelta]): List[HairPixelDelta]={
         val next=getNextPixels( currentpixel, hairs, deltas).filter(_.hp.prHair>0.1d).sortBy { -_.hp.prHair }.headOption
         
-        println("nextPixel:"+next)
-        
         val result= currentPixel ++ next
         
         next match{
@@ -115,87 +115,124 @@ class testRegroupFuzzyHairPixel extends WordSpec {
       }
       
       
+      def consumeHairs(allthepixels: DeltaHairs, deltaRange: Range, imgDst: ImagePlus, currentId: Int=0):Unit={
+        if( currentId > 30){
+          IJ.save( imgDst , DIRECTORY_RESULTIMAGE + s"full_Hairs.tif")
+        }
+        else{
+          val optStartPixel=getFirstHairPixel(  deltaRange, allthepixels)
+          optStartPixel.map{startPixel=>
+            
+            println(s"Start point for id$currentId, $startPixel")
+            
+            val ourhairpixels=consume( startPixel,allthepixels, deltaRange, List(startPixel) )            
+            val img_oneHaire=createBlankImage( "oneHairGrouping", imgDst)
+            val pixeltoremove=removeHairPixels( ourhairpixels,allthepixels)
+            
+            val proc_fullimg = imgDst.getProcessor
+            val proc= img_oneHaire.getProcessor
+            proc.setColor(255)
+            proc_fullimg.setColor(255)
+            pixeltoremove.map{
+               pixels =>
+                 proc.drawPixel(pixels.hp.x, pixels.hp.y.get)
+                 proc_fullimg.drawPixel(pixels.hp.x, pixels.hp.y.get)
+                 
+            }
+            IJ.save( img_oneHaire , DIRECTORY_RESULTIMAGE + s"${currentId}_Hair.tif")
+            
+            val updatemapDeltaHairPixels = cleanHairsPixels( allthepixels, pixeltoremove) 
+            
+            consumeHairs( updatemapDeltaHairPixels, deltaRange, imgDst, currentId+1)
+          }                   
+        }
+      }
+      
+      
       "using first hairpixel at 1.0d pr" in {
         val img=IJ.openImage("/home/sosthene/Downloads/crop_wt_me10_2.tif")
         val deltaRange=(-220 to 100 by 1)
         val mapDeltaHairPixels=hairsCount(img,deltaRange)(new fuzzySearchOutsidersPixelUsingMultiplePolyRegression(50,1))
         
         
-        //start with first pixel on the higher delta       
-        val optStartPixel = getFirstHairPixel(  deltaRange, mapDeltaHairPixels)
-        println("optStartPixel:"+optStartPixel )
-        
-        optStartPixel.map{startPixel=>
-          val ourhairpixels=consume( startPixel,mapDeltaHairPixels, deltaRange, List(startPixel) )
-          println("Pixels size:"+ourhairpixels.size)
+        consumeHairs(mapDeltaHairPixels, deltaRange, img.copyToNewImg("workingcopy"))
+//        
+//        
+//        //start with first pixel on the higher delta       
+//        val optStartPixel = getFirstHairPixel(  deltaRange, mapDeltaHairPixels)
+//        println("optStartPixel:"+optStartPixel )
+//        
+//        optStartPixel.map{startPixel=>
+//          val ourhairpixels=consume( startPixel,mapDeltaHairPixels, deltaRange, List(startPixel) )
+//          println("Pixels size:"+ourhairpixels.size)
+//          
+//          //draw it
+//          val img_oneHaire=createBlankImage( "oneHairGrouping", img)
+//          val proc= img_oneHaire.getProcessor
+//          proc.setColor(255)
+//          
+//          ourhairpixels.map{
+//             pixels =>
+//               proc.drawPixel(pixels.hp.x, pixels.hp.y.get)
+//               
+//          }
+//          IJ.save( img_oneHaire , DIRECTORY_RESULTIMAGE + s"oneHair.tif")
+//          
+//          //Cleaning:
+//          val pixeltoremove=removeHairPixels( ourhairpixels,mapDeltaHairPixels)
+//          proc.setColor(0)
+//          proc.fill()
+//          proc.setColor(255)
+//          pixeltoremove.map{
+//             pixels =>
+//               proc.drawPixel(pixels.hp.x, pixels.hp.y.get)
+//               
+//          }
+//          IJ.save( img_oneHaire , DIRECTORY_RESULTIMAGE + s"removeoneHair.tif")
+//          
+//          
+//          //test remove
+//          val img_removeoneHaire=createBlankImage( "oneHairGrouping", img)
+//          val proctest= img_removeoneHaire.getProcessor
+//          proctest.setColor(RED)
+//          proctest.fill()
+//          proctest.setColor(BLACK)
+//          cleanHairsPixels( mapDeltaHairPixels, pixeltoremove).toList.map(_._2).flatten.foreach{ hairpixel =>
+//            proctest.drawPixel(hairpixel.x, hairpixel.y.get)
+//          }
+//          IJ.save( img_removeoneHaire , DIRECTORY_RESULTIMAGE + s"resultRemoveoneHair.tif")
+//          
+//          
+//          //search second hair
+//          
+//          val updatemapDeltaHairPixels = cleanHairsPixels( mapDeltaHairPixels, pixeltoremove) 
+//          //start with first pixel on the higher delta       
+//        val optStartPixel2 = getFirstHairPixel(  deltaRange, updatemapDeltaHairPixels)
+//        println("optStartPixel2:"+optStartPixel2 )
+//        
+//        optStartPixel2.map{startPixel2=>
+//          val ourhairpixels=consume( startPixel2,updatemapDeltaHairPixels, deltaRange , List(startPixel2))
+//          println("Pixels size:"+ourhairpixels.size)
+//          
+//          println("remove delta -213 result:"+pixeltoremove.filter( _.delta == -213))
+//          
+//          println("ourhairpixels delta -213 result:"+ourhairpixels.filter( _.delta == -213))
+//          
+//          
+//          //draw it
+//          val img_oneHaire=createBlankImage( "oneHairGrouping", img)
+//          val proc= img_oneHaire.getProcessor
+//          proc.setColor(255)
+//          
+//          ourhairpixels.map{
+//             pixels =>
+//               proc.drawPixel(pixels.hp.x, pixels.hp.y.get)
+//               
+//          }
+//          IJ.save( img_oneHaire , DIRECTORY_RESULTIMAGE + s"twoHair.tif")
+//          }
           
-          //draw it
-          val img_oneHaire=createBlankImage( "oneHairGrouping", img)
-          val proc= img_oneHaire.getProcessor
-          proc.setColor(255)
-          
-          ourhairpixels.map{
-             pixels =>
-               proc.drawPixel(pixels.hp.x, pixels.hp.y.get)
-               
-          }
-          IJ.save( img_oneHaire , DIRECTORY_RESULTIMAGE + s"oneHair.tif")
-          
-          //Cleaning:
-          val pixeltoremove=removeHairPixels( ourhairpixels,mapDeltaHairPixels)
-          proc.setColor(0)
-          proc.fill()
-          proc.setColor(255)
-          pixeltoremove.map{
-             pixels =>
-               proc.drawPixel(pixels.hp.x, pixels.hp.y.get)
-               
-          }
-          IJ.save( img_oneHaire , DIRECTORY_RESULTIMAGE + s"removeoneHair.tif")
-          
-          
-          //test remove
-          val img_removeoneHaire=createBlankImage( "oneHairGrouping", img)
-          val proctest= img_removeoneHaire.getProcessor
-          proctest.setColor(RED)
-          proctest.fill()
-          proctest.setColor(BLACK)
-          cleanHairsPixels( mapDeltaHairPixels, pixeltoremove).toList.map(_._2).flatten.foreach{ hairpixel =>
-            proctest.drawPixel(hairpixel.x, hairpixel.y.get)
-          }
-          IJ.save( img_removeoneHaire , DIRECTORY_RESULTIMAGE + s"resultRemoveoneHair.tif")
-          
-          
-          //search second hair
-          
-          val updatemapDeltaHairPixels = cleanHairsPixels( mapDeltaHairPixels, pixeltoremove) 
-          //start with first pixel on the higher delta       
-        val optStartPixel2 = getFirstHairPixel(  deltaRange, updatemapDeltaHairPixels)
-        println("optStartPixel2:"+optStartPixel2 )
-        
-        optStartPixel2.map{startPixel2=>
-          val ourhairpixels=consume( startPixel2,updatemapDeltaHairPixels, deltaRange , List(startPixel2))
-          println("Pixels size:"+ourhairpixels.size)
-          
-          println("remove delta -213 result:"+pixeltoremove.filter( _.delta == -213))
-          
-          println("ourhairpixels delta -213 result:"+ourhairpixels.filter( _.delta == -213))
-          
-          
-          //draw it
-          val img_oneHaire=createBlankImage( "oneHairGrouping", img)
-          val proc= img_oneHaire.getProcessor
-          proc.setColor(255)
-          
-          ourhairpixels.map{
-             pixels =>
-               proc.drawPixel(pixels.hp.x, pixels.hp.y.get)
-               
-          }
-          IJ.save( img_oneHaire , DIRECTORY_RESULTIMAGE + s"twoHair.tif")
-          }
-          
-        }
+ //       }
       }
     }
   }
